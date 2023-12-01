d380bef4c3ea97d4609520af1b5db8edd020d0e9
gabriel <ponderous1734@gmail.com>
2023-12-01 10:35:52 -0300

    Todos os templates da versão 1

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,11 @@ app_name = 'usuarios'
 urlpatterns = [
     path('<int:usuarios_id>/', views.usuarios, name='usuarios'),
     path('', views.index, name='index'),
-    path('contato', views.contato, name='contato') , 
+    path('contato/', views.contato, name='contato') ,
+    path('login/', views.login, name='login'),
+    path('register1/', views.register1, name='register1'),
+    path('register2/', views.register2, name="register2"),
+    path('services/', views.services, name='services'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('mobile/', views.mobile, name='mobile'),
 ]
--- usuarios/views/usuarios_views.py
+++ usuarios/views/usuarios_views.py
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404, render
 def index(request):
     context = {
         'site_title':'Página principal',
+        'header':'Yupix Brasil',
+        'descripition':'Seja bem-vindo ao site da maior empresa de serviços terceirizados no mercado!',
     }
 
     return render(
@@ -17,6 +19,8 @@ def index(request):
 def contato(request):
     context = {
         'site_title':'Contato',
+        'header':'Contato',
+        'descripition':'Entre em contato conosco, oferecemos todo tipo de suporte aos nossos usuários, e se puder deixe seu comentário e avalie-nos.',
     }
 
     return render(
@@ -24,6 +28,63 @@ def contato(request):
         'usuarios/contato.html',
         context,
     )
+
+def login(request):
+
+    return render(
+        request,
+        'usuarios/login.html',
+    )
+
+def register1(request):
+    return render(
+        request,
+        'usuarios/register1.html',
+    )
+
+def register2(request):
+    return render(
+        request,
+        'usuarios/register2.html'
+        )
+
+def services(request):
+    context = {
+        'site_title':'Serviços',
+        'header':'Serviços',
+        'descripition':'Conheço abaixo os principais tipos de serviços que a Yupix oferece para seus usuários!',
+    }
+
+    return render(
+        request,
+        'usuarios/services.html',
+        context,)
+
+def aboutus(request):
+     context = {
+         'Site_title':'Sobre',
+         'header':'Sobre nós',
+         'descripition':'Conheça um pouco sobre nossa empresa, como funciona o aplicativo da Yupix e nossas parcerias mais importantes!'
+     }
+
+     return render(
+         request,
+         'usuarios/aboutus.html',
+         context,
+         )
+
+def mobile(request):
+
+    context = {
+        'site_title':"Mobile",
+        'header':"Serviços",
+        'descripition':"Conheça abaixo os tipos de serviços que a Yupix oferece para seus usuários!",
+    }
+
+    return render(
+        request, 
+        'usuarios/mobile.html', 
+        context)
 
 def usuarios(request, contact_id):
     ...
